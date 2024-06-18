22e55e893b0efd2636064219439884c6358e2ad6
Ciara Carey <84123925+ciaracarey@users.noreply.github.com>
2024-06-18 15:35:49 +0100

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,10 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+import com.google.common.collect.ImmutableList;
+
+public class App {
+    public static void main(String[] args) {
+        ImmutableList<String> list = ImmutableList.of("Hello", "World");
+        System.out.println(list);
+   
+
